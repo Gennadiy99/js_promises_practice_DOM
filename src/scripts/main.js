74b28405636708c3399c59eebf345b84a1949906
Gennadiy99 <login841@gmail.com>
2025-11-12 00:50:57 +0100

    finished project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,82 @@
 'use strict';
+
+const bodyField = document.querySelector('body');
+
+const firstPromise = new Promise((resolve, reject) => {
+  bodyField.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+  setTimeout(reject, 3000, 'First promise was rejected');
+});
+
+firstPromise
+  .then((message) => {
+    const divProm1 = document.createElement('div');
+
+    divProm1.setAttribute('data-qa', 'notification');
+    divProm1.innerHTML = message;
+    divProm1.classList.remove('error');
+    divProm1.classList.add('success');
+
+    bodyField.append(divProm1);
+  })
+  .catch((error) => {
+    const divError1 = document.createElement('div');
+
+    divError1.setAttribute('data-qa', 'notification');
+    divError1.innerHTML = error;
+    divError1.classList.remove('success');
+    divError1.classList.add('error');
+
+    bodyField.append(divError1);
+  });
+
+const secondPromise = new Promise((resolve, reject) => {
+  bodyField.addEventListener('mousedown', () => {
+    resolve('Second promise was resolved');
+  });
+});
+
+secondPromise.then((message) => {
+  const divProm2 = document.createElement('div');
+
+  divProm2.setAttribute('data-qa', 'notification');
+  divProm2.innerHTML = message;
+  divProm2.classList.remove('error');
+  divProm2.classList.add('success');
+
+  bodyField.append(divProm2);
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let leftClick = false;
+  let rightClick = false;
+
+  bodyField.addEventListener('click', () => {
+    leftClick = true;
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+
+  bodyField.addEventListener('contextmenu', (ev) => {
+    ev.preventDefault();
+    rightClick = true;
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+thirdPromise.then((message) => {
+  const divProm3 = document.createElement('div');
+
+  divProm3.setAttribute('data-qa', 'notification');
+  divProm3.innerHTML = message;
+  divProm3.classList.remove('error');
+  divProm3.classList.add('success');
+
+  bodyField.append(divProm3);
+});
